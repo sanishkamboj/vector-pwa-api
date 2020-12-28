3036b34eed81a1c2136cd4ee6caeb2658b8b4923
Sanish <sanish.kamboj@gmail.com>
2020-12-28 14:50:10 +0530

    new changes

--- app/Site.php
+++ app/Site.php
@@ -12,7 +12,7 @@ class Site extends Model
 
     public static function getSiteData($offset){
         $data = DB::select(DB::raw('SELECT "iSiteId" as siteid, "iSTypeId" as sTypeId, "iCityId", "iZoneId", st_astext(ST_Centroid("vPolygonLatLong")) as polyCenter, st_astext("vPolygonLatLong") as polygon, st_astext("vPointLatLong") as point, st_astext("vPolyLineLatLong") as poly_line FROM site_mas Where "iSTypeId" IN(Select "iSTypeId" FROM site_type_mas) AND "iStatus" = 1 LIMIT 1000 OFFSET '.$offset));
-        dd($data);
+        //dd($data);
         return $data;
     }
 }
