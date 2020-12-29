29a18bf0de46394dff0877679989dd2b3a1ff2ab
Sanish <sanish.kamboj@gmail.com>
2020-12-29 15:07:43 +0530

    new changes

--- app/Zone.php
+++ app/Zone.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Zone extends Model
 {
-    //
+    protected $table = 'zone';
+    protected $primaryKey = 'iZoneId';
+    public $timestamps = false;
 }
